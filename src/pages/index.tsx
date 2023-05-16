7a64e0ac8eda7174de5509db2ffbee5d18e8944f
renaanpio <renanps1@hotmail.com>
2023-05-15 23:50:51 -0300

    configure global-styles and theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import Head from 'next/head';
 import styled from 'styled-components';
 
 export const Wrapper = styled.div``;
 
-export const Title = styled.h1`
-  color: 'red';
+export const Title = styled.title``;
+
+export const SubTitle = styled.h1`
+  color: ${({ theme }) => theme.colors.secondaryColor};
+  display: flex;
 `;
 
 export default function Home() {
   return (
-    <Wrapper>
-      <Title> Ola rtask </Title>
-    </Wrapper>
+    <>
+      <Head>
+        <Title>Rtask - Home</Title>
+      </Head>
+      <Wrapper>
+        <Head>
+          <Title>Rtask</Title>
+        </Head>
+        <SubTitle> Ola rtask </SubTitle>
+      </Wrapper>
+    </>
   );
 }
---
+++ src/styles/theme.tsx
@@ -0,0 +1,40 @@
+export const theme = {
+  colors: {
+    primaryColor: '#F2F6FC',
+    secondaryColor: '#141A29',
+    alert: '#FFB800',
+    mediumGray: '#DDDDDD',
+    warning: '#FF3636',
+    primaryGreen: '#04DD75',
+    secondaryGreen: '#04DD75',
+  },
+  font: {
+    family: {
+      default: "'Open Sans', sans-serif",
+      secondary: "'Montserrat', sans-serif",
+    },
+    sizes: {
+      xsmall: '8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      huge: '5.6rem',
+      xhuge: '6.4rem',
+    },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
+  },
+  spacings: {
+    xsmall: '8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.0rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
+    xhuge: '6.4rem',
+  },
+};
